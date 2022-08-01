db9a88c1d1727864693a3d7c9c334511d3e98607
Daniel Moya <dmalvarado94@gmail.com>
2022-08-01 01:35:23 -0400

    first commit

---
+++ bot/bot.go
@@ -0,0 +1,47 @@
+package bot
+
+import (
+	"fmt"
+
+	"github.com/akhil/discord-ping/config"
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotID string
+var goBot *discordgo.Session
+
+func Start() {
+	goBot, err := discordgo.New("Bot " + config.Token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+
+	}
+
+	u, err := goBot.User("@me")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	BotID = u.ID
+
+	goBot.AddHandler(messageHandler)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Bot Valorant Hub is running!")
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == BotID {
+		return
+	}
+
+	if m.Content == "ping" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+}
